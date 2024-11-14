7d93c330a5d286d0b4d00d33a117ccc5c4a30122
shivam khode <130055547+shivam-khode01@users.noreply.github.com>
2024-11-14 23:18:04 +0530

    Update OurTeam.jsx

--- src/components/OurTeam/OurTeam.jsx
+++ src/components/OurTeam/OurTeam.jsx
@@ -17,7 +17,7 @@ const OurTeam = () => {
     <div className="min-h-screen bg-black text-white">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-16 sm:py-24">
         <TeamHeader />
-        <CoreTeamSection members={coreTeamMembers} />
+        {/*<CoreTeamSection members={coreTeamMembers} />*/}
         {/* <BestTeam /> */}
         <OtherTeamsSection
           title="Technical Team"
