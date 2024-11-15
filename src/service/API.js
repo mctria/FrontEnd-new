18fefc2bab4fdf35a25154915133dbaebfbb02c4
Celcilin (Web Dev) <102462550+mctria@users.noreply.github.com>
2024-11-15 16:43:23 +0530

    Update API.js

--- src/service/API.js
+++ src/service/API.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const uri = "192.168.1.6";
-const BASE_URL = `https://aces-api.onrender.com`;
+const BASE_URL = process.env.URL || `https://aces-api.onrender.com`;
 
 var Endpoint = {
   Blog: "/blog",
